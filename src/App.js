63ba460f78fa2e5635025df260bbc43f3f1fd825
SahilBas2005 <sahilbas2005@gmail.com>
2025-05-24 20:30:25 -0400

    Update project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import { assets } from "./assets/assets.js";
 
-function App() {
+const App = () =>{
+  const [progress, setProgress] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-text">TEST</h1>
+        <div className="logo">
+        <img src={assets.react_logo} alt="React logo" />
+        <p>React</p>
+      </div>
+      <div className="home">
+        <p onClick={() => setProgress((prev) => ((prev + 1)%assets.home_text.length))}>{assets.home_text[progress]}</p>
+        <p>{progress}</p>
+      </div>
       </header>
     </div>
   );
---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import react_logo from "./react_logo.png";
+
+const home_text = [
+    "First line of text.",
+    "Second line of text.",
+    "Third line of text.",
+];    
+
+export const assets = {
+    react_logo,
+    home_text
+}
